3f0ffbad80f90baa24ee0838cadb5651113c3cf2
Vadim <vadim_kimm@icloud.com>
2023-08-19 13:11:26 +0300

    Create project structure

--- RickAndMorty/Sources/Scenes/MainViewController.swift
+++ RickAndMorty/Sources/Scenes/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  RickAndMorty
 //
 //  Created by Vadim Kim on 19.08.2023.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
     }
-
-
 }
-
